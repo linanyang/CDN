d44bc1615647266f4cf4069c4de0a99debab4876
linanyang <nanyeoe@gmail.com>
2020-08-11 11:46:42 +0800

    change

--- theme_butterfly/js/Lete.js
+++ theme_butterfly/js/Lete.js
@@ -4,9 +4,9 @@
 
 
 /*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+// var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
 /*添加到返回顶部按钮下*/
-document.getElementById("go-up").innerHTML += top_up;
+// document.getElementById("go-up").innerHTML += top_up;
 
 /* 手机客户端导航栏默认隐藏 */
 var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
@@ -37,7 +37,8 @@ document.addEventListener('visibilitychange', function() {
 
 
 // 气泡
-function qipao() {
+/*
+ function qipao() {
     $('#page-header').circleMagic({
         radius: 10,
         density: .2,
@@ -81,9 +82,10 @@ function qipao() {
     }
 }(jQuery);
 qipao()
+*/
 
 
 // 设置页脚博主
 $(document).ready(function(e) {
-    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Lete乐特');
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> 南阳');
 });
