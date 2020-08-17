52a47f00015b0ada1b4f77e4f4a7169c39ed15f9
linanyang <nanyeoe@gmail.com>
2020-08-17 18:36:02 +0800

    Update fish.js

--- theme_butterfly/js/fish.js
+++ theme_butterfly/js/fish.js
@@ -142,7 +142,7 @@ var RENDERER = {
 		requestAnimationFrame(this.render);
 		this.controlStatus();
 		this.context.clearRect(0, 0, this.width, this.height);
-		this.context.fillStyle = 'hsla(0, 0%, 95% .7)';
+		this.context.fillStyle = 'hsla(0, 0%, 95%, .7)';
 		
 		for(var i = 0, count = this.fishes.length; i < count; i++){
 			this.fishes[i].render(this.context);
