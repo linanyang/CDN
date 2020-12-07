7d829111cb0bc963546faf7a854f9959dafaf097
linanyang <nanyeoe@gmail.com>
2020-12-07 10:00:41 +0800

    Update Nanyang.js

--- theme_butterfly/js/Nanyang.js
+++ theme_butterfly/js/Nanyang.js
@@ -1,4 +1,4 @@
 // 设置页脚博主
 $(document).ready(function(e) {
-    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> 南阳');
+    $('.copyright').html('©2020 <i style="color:#FF6A6A" class="fa fa-heartbeat"></i> 南阳');
 });
